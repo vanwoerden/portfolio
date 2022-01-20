8e7062a081054ac0f3a5366448e5f9bddad21530
Ernst <hello@seriousness.nl>
2022-01-20 11:32:37 -0500

    font switch 1

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -18,6 +18,43 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width,initial-scale=1.0">
 
+  <meta property="og:title" content="The Works of Ernst">
+  <meta property="og:description" content="Ernst van Woerden is a designer.">
+  <meta property="og:image" content="https://ernst.works/img/twitter_card.png">
+  <meta property="og:url" content="https://ernst.works">
+  <meta name="twitter:card" content="summary_large_image">
+  
+  <!--  Non-Essential, But Recommended -->
+  <meta property="og:site_name" content="Ernst.works">
+  <meta name="twitter:image:alt" content="Ernst is a designer.">
+  
+  <!--  Non-Essential, But Required for Analytics -->
+  <meta name="twitter:site" content="@erniedesigns">    
+  <script>
+      window['_fs_debug'] = false;
+      window['_fs_host'] = 'fullstory.com';
+      window['_fs_script'] = 'edge.fullstory.com/s/fs.js';
+      window['_fs_org'] = '14NPER';
+      window['_fs_namespace'] = 'FS';
+      (function(m,n,e,t,l,o,g,y){
+          if (e in m) {if(m.console && m.console.log) { m.console.log('FullStory namespace conflict. Please set window["_fs_namespace"].');} return;}
+          g=m[e]=function(a,b,s){g.q?g.q.push([a,b,s]):g._api(a,b,s);};g.q=[];
+          o=n.createElement(t);o.async=1;o.crossOrigin='anonymous';o.src='https://'+_fs_script;
+          y=n.getElementsByTagName(t)[0];y.parentNode.insertBefore(o,y);
+          g.identify=function(i,v,s){g(l,{uid:i},s);if(v)g(l,v,s)};g.setUserVars=function(v,s){g(l,v,s)};g.event=function(i,v,s){g('event',{n:i,p:v},s)};
+          g.anonymize=function(){g.identify(!!0)};
+          g.shutdown=function(){g("rec",!1)};g.restart=function(){g("rec",!0)};
+          g.log = function(a,b){g("log",[a,b])};
+          g.consent=function(a){g("consent",!arguments.length||a)};
+          g.identifyAccount=function(i,v){o='account';v=v||{};v.acctId=i;g(o,v)};
+          g.clearUserCookie=function(){};
+          g.setVars=function(n, p){g('setVars',[n,p]);};
+          g._w={};y='XMLHttpRequest';g._w[y]=m[y];y='fetch';g._w[y]=m[y];
+          if(m[y])m[y]=function(){return g._w[y].apply(this,arguments)};
+          g._v="1.3.0";
+      })(window,document,window['_fs_namespace'],'script','user');
+  </script>
+
   <?php
   /*
     In the title tag we show the title of our
@@ -36,6 +73,7 @@
   <?= css([
     'assets/css/prism.css',
     'assets/css/lightbox.css',
+    'assets/css/fonts.css',
     'assets/css/index.css',
     '@auto'
   ]) ?>

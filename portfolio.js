e9478c7124f0358470519f55d33c034b4256555a
Ernst van Woerden <hello@seriousness.nl>
2024-01-18 20:29:20 -0500

    refresh
    new year new me

---
+++ portfolio.js
@@ -0,0 +1,63 @@
+// document.addEventListener("DOMContentLoaded", (event) => {
+    console.log("DOM fully loaded and parsed");
+    var timeToggle = document.getElementById("timeToggle");
+    var scopeToggle = document.getElementById("scopeToggle");
+    console.log("hi");
+
+    document.querySelectorAll('.nav-link').forEach((elem) => {
+        elem.addEventListener('click', handleClickLink);
+        console.log(elem+ " listener added");
+    });
+// });
+
+    function handleClickLink(event) {
+        event.preventDefault();
+        const block = event.target.dataset.link;
+        console.log(event.target);
+        console.log("block: "+block);
+        document.getElementById(block).scrollIntoView({ behavior: ""});
+    }
+
+    function fixedTime(){
+        console.log("fixedTime");
+        var timeToggle = document.getElementById("time");
+        var scopeToggle = document.getElementById("scope");
+
+        scopeToggle.checked = false;
+    }
+
+    function fixedScope(){
+        console.log("fixedScope");
+        var timeToggle = document.getElementById("time");
+        var scopeToggle = document.getElementById("scope");
+        //console.log(timeToggle);
+
+        timeToggle.checked = false;
+    }
+
+    var rotated = false;
+    function rotateBML() {
+        console.log("rotateBML");
+        document.getElementById("bml-container").classList.add("rotated");
+        document.getElementById("build").classList.add("rotated-element");
+        document.getElementById("measure").classList.add("rotated-element");
+        document.getElementById("learn").classList.add("rotated-element");
+        document.getElementById("learn").innerHTML = "Learn first";
+        document.getElementById("build").innerHTML = "Then build";
+        document.getElementById("measure").innerHTML = "Then measure";
+        document.getElementById("reset").innerHTML = "(reset)";
+        document.getElementById("bml-title").innerHTML = "Learn, build, measure.";
+        rotated = true;
+    }
+
+    function rotateBMLBack() {
+        document.getElementById("bml-container").classList.remove("rotated");
+        document.getElementById("build").classList.remove("rotated-element");
+        document.getElementById("measure").classList.remove("rotated-element");
+        document.getElementById("learn").classList.remove("rotated-element");
+        document.getElementById("learn").innerHTML = "Learn";
+        document.getElementById("build").innerHTML = "Build";
+        document.getElementById("measure").innerHTML = "Measure";
+        document.getElementById("reset").innerHTML = "";
+        document.getElementById("bml-title").innerHTML = "Build, measure, learn?";
+    }
